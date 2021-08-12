b9cdc6a6dda8572fd996d2a3ed6a661b9140dd54
QAIgorU <86653064+QAIgorU@users.noreply.github.com>
2021-08-12 15:00:10 +0300

    Update conftest.py
    new

--- conftest.py
+++ conftest.py
@@ -1,10 +1,31 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#@pytest.fixture
+#def browser(request):
+    #driver = webdriver.Chrome(capabilities={"unexpectedAlertBehaviour": "dismiss"})
+    #request.addfinalizer(driver.quit)
+    #print(driver.capabilities)
+    #return driver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
 
 @pytest.fixture(scope="function")
-def browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
